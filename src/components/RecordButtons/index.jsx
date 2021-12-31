cd57a8460b818280e2a497ac5a76ade7305d4891
Yitz <chalfari@gmail.com>
2021-12-31 21:50:52 +0200

    trying to fix recording bug

---
+++ src/components/RecordButtons/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecordButtons = () => {
+  return <div></div>;
+};
+
+export default RecordButtons;
--- src/components/Synth/index.jsx
+++ src/components/Synth/index.jsx
@@ -31,31 +31,74 @@ const Synth = () => {
     const now = Tone.now();
     let int = 0;
     arr.forEach((element) => {
-      const { note, interval } = element;
+      const { note, interval, octave, timing } = element;
       // console.log(note, interval, int);
       int = interval;
       console.log(int, recorded.current);
       // setTimeout(() => {
       //   synth.triggerAttackRelease(`${note}`, "4n");
       // }, `${int}`);
-      synth.triggerAttackRelease(`${note}`, "8n", `${now + int}`);
+      synth.triggerAttackRelease(
+        `${note}${octave}`,
+        `${timing}n`,
+        `${now + int}`
+      );
     });
   };
 
   const handleKeyDown = (key) => {
     const noteObj = all.filter((note) => note.key === key.toLowerCase())[0];
-
     if (noteObj.key === key.toLowerCase()) {
       console.log(noteObj.note, noteObj.octave);
       synth.triggerAttackRelease(
         `${noteObj.note}${noteObj.octave}`,
         `${noteObj.timing}n`
       );
+      if (recording.current) {
+        noteObj.interval = currInterval;
+        recorded.current.push(noteObj);
+        setRecordedArr(recorded.current);
+        // console.log(recorded.current, recording);
+        clearInterval(startCurrInt);
+        currInterval = 0;
+        startCurrInt();
+      }
     }
   };
   return (
-    <div>
+    <div className="synth">
       <Keyboard notesToRender={notesToRender} synth={synth} />
+      <div className="record-buttons">
+        <button
+          onClick={() => {
+            if (recording.current) {
+              clearInterval(startCurrInt);
+              console.log(currInterval);
+              currInterval = 0;
+            }
+
+            setisRecording(!isRecording);
+            recording.current = !recording.current;
+          }}
+        >
+          {recording.current ? "Stop" : "Record"}
+        </button>
+        {recordedArr.length > 0 && (
+          <>
+            <button
+              onClick={() => {
+                recorded.current = [];
+                setRecordedArr(recorded.current);
+              }}
+            >
+              Scrap Recording
+            </button>
+            <button onClick={() => handlePlayRecording(recorded.current)}>
+              Play Recording
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
