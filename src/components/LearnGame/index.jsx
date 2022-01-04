5e84abe146da8646264a0bdaab1490a07d44192d
Yitz <chalfari@gmail.com>
2022-01-04 08:03:54 +0200

    func to calc accuracy and print message

--- src/components/LearnGame/index.jsx
+++ src/components/LearnGame/index.jsx
@@ -7,7 +7,30 @@ const LearnGame = ({ selectedSong }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [countdownTimer, setCountdownTimer] = useState(null);
   const [gameMessage, setGameMessage] = useState("");
+  const [accuracyMessage, setAccuracyMessage] = useState("");
+  const gameResults = useRef([]);
+  const [difficulty, setDifficulty] = useState(0.5);
   const currIndex = useRef(0);
+
+  const calculateTimeAccuracy = (notePressed, songNote, leeway) => {
+    const accuracy = {};
+    switch (true) {
+      case notePressed.time > songNote.time + leeway:
+        accuracy.time = "Too Slow";
+        accuracy.slow = true;
+        break;
+      case notePressed.time < songNote.time - leeway:
+        console.log(notePressed.time, songNote.time);
+        accuracy.time = "Too Soon";
+        accuracy.soon = true;
+        break;
+      default:
+        accuracy.time = "Great Timing!";
+        accuracy.accurate = true;
+    }
+    return accuracy;
+  };
+
   const startGame = () => {
     setCountdownTimer(3);
     const countdown = setInterval(() => {
@@ -16,6 +39,7 @@ const LearnGame = ({ selectedSong }) => {
     setTimeout(() => {
       clearInterval(countdown);
       setCountdownTimer("GO!");
+      setIsPlaying((isPlaying) => !isPlaying);
       Tone.Transport.start();
       window.addEventListener("keydown", (e) => {
         const key = e.key;
@@ -25,35 +49,49 @@ const LearnGame = ({ selectedSong }) => {
         if (noteObj.key === key.toLowerCase()) {
           const newObj = { ...noteObj };
           const songNote = selectedSong[currIndex.current];
-          const songTime = selectedSong[currIndex.current].time - 0.969;
+          // const songTime = selectedSong[currIndex.current].time - 0.969;
+          songNote.time = songNote.time - 0.969;
           newObj.time = Tone.Transport.seconds.toFixed(3);
-          console.log(songNote.note, songTime, newObj.note, newObj.time);
-          if (
-            newObj.time >= songTime - 0.5 &&
-            newObj.time <= songTime + 0.5 &&
-            newObj.note === songNote.note
-          ) {
+          console.log(songNote.note, songNote.time, newObj.note, newObj.time);
+          const timeMessage = calculateTimeAccuracy(
+            newObj,
+            songNote,
+            difficulty
+          );
+
+          setAccuracyMessage(timeMessage.time);
+          if (newObj.note === songNote.note) {
             setGameMessage("GOOD JOB!");
+            timeMessage.correctNote = true;
           } else {
-            setGameMessage("WHUPS");
+            setGameMessage("Wrong note!");
+            timeMessage.correctNote = false;
           }
-          console.log(currIndex.current);
+          gameResults.current.push(timeMessage);
           //need a way to time the incrementation of currIndx
           currIndex.current++;
+
           // setTimeout(() => {
           //   currIndex.current++;
           //   console.log(currIndex.current);
           // }, (selectedSong[currIndex.current + 1].time - 0.569 - Tone.Transport.seconds) * 1000);
         }
       });
+      setTimeout(() => {
+        console.log(gameResults.current);
+        setIsPlaying((isPlaying) => !isPlaying);
+      }, selectedSong[selectedSong.length - 1].time * 1000);
     }, 3000);
   };
   //Loop through songArr and schedule timeouts to change current index or set whatever is needed
   return (
     <div className="learn-game">
       <h3 className="game-countdown">{countdownTimer && countdownTimer}</h3>
-      <Button onClick={startGame} type="text" text="Play" />
-      <div className="game-message">{gameMessage}</div>
+      <Button onClick={startGame} type="text" text="Start Game" />
+      {isPlaying && (
+        <div className="game-message">{`Note: ${gameMessage} Timing: ${accuracyMessage}`}</div>
+      )}
+      {isPlaying && "We're Playing"}
     </div>
   );
 };
